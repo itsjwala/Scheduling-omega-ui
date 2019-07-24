3363d8a4d5fab28a4bdf9e22c27667aa448c2144
ramraj666 <pmaurya196@gmail.com>
2019-07-24 19:58:25 +0530

    reports finished and export csv

--- src/app/commons/commons-routing.module.ts
+++ src/app/commons/commons-routing.module.ts
@@ -1,3 +1,4 @@
+import { HrReportComponent } from './hr-report/hr-report.component';
 import { AboutViewComponent } from './about-view/about-view.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -5,6 +6,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PreviousScheduleComponent } from './previous-schedule/previous-schedule.component';
 import { FullCalendar, FullCalendarModule } from 'primeng/fullcalendar';
+import { ReportsComponent } from './reports/reports.component';
+import { InterviewerReportComponent } from './interviewer-report/interviewer-report.component';
 
 const commonRoutes: Routes = [
     {
@@ -13,7 +16,10 @@ const commonRoutes: Routes = [
         children:
             [
                 { path: 'previous', component: PreviousScheduleComponent },
-                { path: 'portfolio', component: AboutViewComponent }
+                { path: 'portfolio', component: AboutViewComponent },
+                { path: 'reports', component: ReportsComponent},
+                { path: 'interviewer-reports', component: InterviewerReportComponent},
+                { path: 'hr-reports', component: HrReportComponent}
             ]
     }
 ];
---
+++ src/app/commons/hr-report/hr-report.component.ts
@@ -0,0 +1,67 @@
+import { AppConstants } from './../../AppConstants';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from "ngx-spinner";
+import * as moment from "moment";
+
+@Component({
+  selector: 'app-hr-report',
+  templateUrl: './hr-report.component.html',
+  styleUrls: ['./hr-report.component.css']
+})
+export class HrReportComponent implements OnInit {
+
+  // reportHR = [];
+  to = '';
+  from = '';
+
+  columnDefs = [
+    { headerName: 'name', field: 'hrName', sortable: true, filter: true },
+    { headerName: 'email', field: 'hrEmail', sortable: true, filter: true },
+    { headerName: 'wissenId', field: 'hrWissenId', sortable: true, filter: true },
+    { headerName: 'phoneNumber', field: 'hrPhone', sortable: true, filter: true },
+    { headerName: 'active', field: 'hrActive', sortable: true, filter: true },
+    { headerName: 'count', field: 'count', sortable: true, filter: true }
+  ];
+
+  gridOptions = {
+    defaultColDef: {
+      resizable: true,
+    },
+    columnDefs: this.columnDefs,
+    // onGridReady(params) {
+    //   this.gridApi = params.api;
+    //   this.gridColumnApi = params.columnApi;
+    //   params.api.sizeColumnsToFit();
+    // },
+  }
+
+  rowData: any;
+
+
+  constructor(private http: HttpClient, private spinner: NgxSpinnerService) { }
+
+  ngOnInit() {
+  }
+
+  toSelected(event){
+    this.to = moment(event.value).format().split('+')[0];
+    // console.log(moment(this.to).format());
+  }
+
+  fromSelected(event){
+    this.from = moment(event.value).format().split('+')[0];
+    if(this.to){
+      this.getReport(this.from, this.to);
+    }
+  }
+
+  getReport(to, from){
+    this.spinner.show();
+    this.http.get(AppConstants.getHRReport(from, to)).subscribe((e: any) => {
+      this.rowData = e;
+      this.spinner.hide();
+    });
+  }
+
+}
